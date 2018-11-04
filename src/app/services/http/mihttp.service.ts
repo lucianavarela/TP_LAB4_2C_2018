511b95d83f520f23482d9ababf086f8ec4c50cf0
lucianavarela <lucianavarela@hotmail.es>
2018-11-03 23:50:58 -0300

    cabmios

--- src/app/services/http/mihttp.service.ts
+++ src/app/services/http/mihttp.service.ts
@@ -2,62 +2,56 @@ import { Injectable, ViewChild } from '@angular/core';
 import { Response } from '@angular/http';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class MihttpService {
   public api: string = "https://comandaback.herokuapp.com/";
-  public httpOptions: any = {};
 
   constructor(public http: HttpClient) {
     let token = localStorage.getItem('token');
-    if (token == 'null') {
-      this.httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      }
-    } else {
-      this.httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'Authorization': token
-        })
-      }
+    if (token != '') {
+      httpOptions.headers = httpOptions.headers.append('Authorization', token);
     }
+    console.log(httpOptions);
   }
 
   public get(url: string): any {
-    return this.http.get(this.api + url, this.httpOptions.headers)
+    return this.http.get(this.api + url, httpOptions)
       .toPromise()
       .then(this.extractData)
       .catch(this.handleError);
   }
 
   public post(url, data: Object) {
-    return this.http.post(this.api + url, data, this.httpOptions.headers)
+    return this.http.post(this.api + url, data, httpOptions)
       .toPromise()
       .then(this.extractData)
       .catch(this.handleError);
   }
 
   public delete(url: string) {
-    return this.http.delete(url, this.httpOptions.headers)
+    return this.http.delete(url, httpOptions)
       .toPromise()
       .then(this.extractData)
       .catch(this.handleError);
   }
 
   public put(url: string, objeto: any) {
-    return this.http.put(url, objeto, this.httpOptions.headers)
+    return this.http.put(url, objeto, httpOptions)
       .toPromise()
       .then(this.extractData)
       .catch(this.handleError);
   }
 
   private extractData(res: any) {
-    console.log(res);
     return res || {};
   }
 
